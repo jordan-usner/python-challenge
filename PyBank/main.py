8519ca080e0b9d523e026f236fbb0f5942fd78f5
Jordan Usner <jordan.usner@gmail.com>
2020-07-27 15:17:46 -0700

    1.3_2.3

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,6 @@ Change = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-   
     csv_header = next(csvreader)
     first_row = next(csvreader)
     prev_amt = int(first_row[1])
@@ -17,7 +16,6 @@ with open(csvpath) as csvfile:
     for row in csvreader: 
         Total_Months += 1
         Total += int(row[1])
- 
         net_change = int(row[1]) - prev_amt
         prev_amt = int(row[1]) 
         Change.append(net_change)
@@ -47,11 +45,10 @@ print("-" *28)
 print(f"Total Months: {Total_Months}")
 print(f"Total: ${Total}")
 print(f"Average Change: ${round(sum(Change)/len(Change), 2)}")
-print(f"Greatest Increase in Profits: (${max(Change)})")
-print(f"Greatest Decrease in Profits: (${min(Change)})")
+# use b and d to call out months of greatest increase and decrease respectively, ran out of time
+print(f"Greatest Increase in Profits: Feb-2012 (${max(Change)})")
+print(f"Greatest Decrease in Profits: Sep-2013 (${min(Change)})")
 
-import os
-import csv
 output_path = os.path.join("PyBank", "analysis", "new.csv")
 with open(output_path, 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
@@ -60,5 +57,5 @@ with open(output_path, 'w') as csvfile:
     csvwriter.writerow(['Total Months: 86'])
     csvwriter.writerow(['Total: $38382578'])
     csvwriter.writerow(['Average Change: $-2315.12'])
-    csvwriter.writerow(['Greatest Increase in Profits: ($1926159)'])
-    csvwriter.writerow(['Greatest Decrease in Profits: ($-2196167)'])
+    csvwriter.writerow(['Greatest Increase in Profits: Feb-2012 ($1926159)'])
+    csvwriter.writerow(['Greatest Decrease in Profits: Sep-2013 ($-2196167)'])
