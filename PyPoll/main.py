0c4b7cad3ed1784595e92d834bc4ac0c8a02ba9a
Jordan Usner <jordan.usner@gmail.com>
2020-07-27 15:01:54 -0700

    2.2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,11 @@ import csv
 csvpath = os.path.join("PyPoll", "Resources", "03-Python_Homework_PyPoll_Resources_election_data.csv")
 Total_Votes = 0
 Candidates = []
+Candidates = list(dict.fromkeys(Candidates))
+Khan = 0
+Correy = 0
+Li = 0
+O_Tooley = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -13,17 +18,30 @@ with open(csvpath) as csvfile:
         Total_Votes += 1
         candidate = row[2]
         Candidates.append(candidate)
-        print(Candidates)
+        if candidate == "Khan":
+            Khan += 1
+        elif candidate == "Correy":
+            Correy += 1
+        elif candidate == "Li":
+            Li += 1
+        elif candidate == "O'Tooley":
+            O_Tooley += 1
+
+Khan_perc = 100*Khan/Total_Votes
+Correy_perc = 100*Correy/Total_Votes
+Li_perc = 100*Li/Total_Votes
+O_Tooley_perc = 100*O_Tooley/Total_Votes
 
 print("Election Results")
 print("-" *25)
 print(f"Total Votes: {Total_Votes}")
 print("-" *25)
-# print(Leader: % (#))
-# print(2nd: % (#))
-# print(3rd: % (#))
-# print(4th: % (#))
+print(f"Khan: {round(Khan_perc, 3)}% {(Khan)}")
+print(f"Khan: {round(Correy_perc, 3)}% {(Correy)}")
+print(f"Khan: {round(Li_perc, 3)}% {(Li)}")
+print(f"Khan: {round(O_Tooley_perc, 3)}% {(O_Tooley)}")
 print("-" *25)
-print("Winner: ")
+print("Winner: Khan")
 # print(f"Winner: {Leader}")
 print("-" *25)
+
